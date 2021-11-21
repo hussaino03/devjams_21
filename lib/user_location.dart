ea306fdec92993db7f0e77cc1081803c538f28b7
Hussain Omer <67332652+hussaino03@users.noreply.github.com>
2021-11-20 20:02:18 -0500

    Update user_location.dart

--- lib/user_location.dart
+++ lib/user_location.dart
@@ -1,105 +1,42 @@
-// import libraries
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import library
 import 'package:geolocator/geolocator.dart';
 
-class DashboardScreen extends StatefulWidget {
-  @override
-  _DashboardState createState() => _DashboardState();
-}
+// create variables
+Position _currentPosition;
+String _currentAddress;
 
-class _DashboardState extends State<DashboardScreen> {
-  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
-  Position _currentPosition;
-  String _currentAddress;
+// function to identify the location of the user
+final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
-  @override
-  void initState() {
-    super.initState();
-    _getCurrentLocation();
-  }
-
-   // Create a function to get current location
-  _getCurrentLocation() {
-    geolocator
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
-        .then((Position position) {
-      setState(() {
-        _currentPosition = position;
-      });
-
-      _getAddressFromLatLng();
-    }).catchError((e) {
-      print(e);
+// get the user location
+_getCurrentLocation() {
+  geolocator
+      .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+      .then((Position position) {
+    setState(() {
+      _currentPosition = position;
     });
-  }
+    _getAddressFromLatLng();
+  }).catchError((e) {
+    print(e);
+  });
+}
 
-    // Address Function
-  _getAddressFromLatLng() async {
-    try {
-      List<Placemark> p = await geolocator.placemarkFromCoordinates(
-          _currentPosition.latitude, _currentPosition.longitude);
-
-      Placemark place = p[0];
-
-      setState(() {
-        _currentAddress =
-            "${place.locality}, ${place.postalCode}, ${place.country}";
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("DASHBOARD"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).canvasColor,
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.location_on),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'Location',
-                                style: Theme.of(context).textTheme.caption,
-                              ),
-                              if (_currentPosition != null &&
-                                  _currentAddress != null)
-                                Text(_currentAddress,
-                                    style:
-                                        Theme.of(context).textTheme.bodyText2),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                      ],
-                    ),
-                  ],
-                ))
-          ],
-        ),
-      ),
-    );
+// get address from coordinates
+_getAddressFromLatLng() async {
+  try {
+    List<Placemark> p = await geolocator.placemarkFromCoordinates(
+        _currentPosition.latitude, _currentPosition.longitude);
+    Placemark place = p[0];
+    setState(() {
+      _currentAddress =
+      "${place.locality}, ${place.postalCode}, ${place.country}";
+    });
+  } catch (e) {
+    print(e);
   }
 }
+
+// display address on screen
+if (_currentPosition != null && _currentAddress != null) Text(_currentAddress),
+  string address = "${place.locality}, ${place.postalCode}, ${place.country}";
